b1bdf6830508218b7238c3b6a8f286fd1050c261
BhawaniRawat <bhawanirawatsoftdev@gmail.com>
2023-05-09 10:54:13 +0530

    userinput

--- first.java
+++ first.java
@@ -1,7 +1,16 @@
-class Bhawani {
-public static void main (String arr[])
-{
-String name = "Bhawani Singh ";
-System.out.println("hello "+name);
-}
+import java.util.*;
+class Demo {
+    public static void main(String arr[]) {
+        int a;
+        float b;
+        String name;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Number Daal : ");
+        a = obj.nextInt();
+        System.out.print("Enter Float Value : ");
+        b = obj.nextFloat();
+        System.out.print("Enter A String");
+        name = obj.next();
+        System.out.println("The Number Is : "+a+"The Floating Number : "+b+"The Name Is : "+name);
+    }
 }
